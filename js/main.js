291e8291e6807f961e05b6eab52faedd66083e4a
Artur Yenatskyi <artur.yenatskyi@smile-ukraine.com>
2022-07-12 14:00:36 +0300

    Added Ispi Pay project pages.

--- js/main.js
+++ js/main.js
@@ -7,6 +7,11 @@ $(function() {
     let menu = $(".navigation-main__primary.desktop .main-menu");
     let pageUrl = window.location.pathname.split("/");
     let activeUrl = pageUrl[pageUrl.length - 1];
+    let showMoreLink = $('#show_more_link');
+    let showMoreProductsLink = $('#products_show_more');
+    let showMoreText = $('.show-more-heading').find('.text-row');
+    let productsBlock = $('.products-block');
+    let showMoreItems = productsBlock.find('.toggle-products');
 
     function activeMenu () {
         menu.find('.active').removeClass('active');
@@ -31,6 +36,26 @@ $(function() {
         });
     }
 
+    function showMore () {
+        showMoreLink.on('click', function (event) {
+            event.preventDefault();
+            showMoreText.toggleClass('visible');
+        });
+    }
+    function showMoreProducts () {
+        showMoreProductsLink.on('click', function (event) {
+            event.preventDefault();
+
+            productsBlock.toggleClass('active');
+            showMoreItems.toggleClass('visible');
+
+            productsBlock[0].scrollIntoView({
+                behavior: "smooth", // or "auto" or "instant"
+                block: "end" // or "end"
+            });
+        });
+    }
+
     $(window).on('scroll', function () {
         let header = $("header");
 
@@ -39,4 +64,6 @@ $(function() {
 
     mobileMenu();
     activeMenu();
+    showMore();
+    showMoreProducts();
 });
